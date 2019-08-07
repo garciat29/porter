6c83fd7b4f81260921a3a57771b7986aa68dbe6a
Tommy Garcia <garciat29@gmail.com>
2019-08-07 00:27:24 +0000

    first neo4j model

--- script/models.py
+++ script/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+#from django.db import models
+from neo4django.db import models
+
+class Script(models.NodeModel):
+    name=models.StringProperty()
+    owners=models.ArrayProperty()
+    input_datasets=models.ArrayProperty()
+    #change to relationship tp a dataset node
+    output=models.IntegerProperty()
+    file_path=models.StringProperty()
 
 # Create your models here.
