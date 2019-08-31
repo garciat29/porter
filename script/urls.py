8d7306a739f69e1004d76902c6a9a7c78f081981
Tommy Garcia <garciat29@gmail.com>
2019-08-31 17:37:01 +0000

    write and exec

--- script/urls.py
+++ script/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<str:project_name>/<int:script_id>', views.index, name='index'),
+    path('<str:project_name>/<int:script_id>/run_script',views.run_script, name='run_script'),
 ]
--- script/views.py
+++ script/views.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from django.shortcuts import get_object_or_404, render, redirect 
 from django.http import HttpResponse, HttpResponseRedirect
 
@@ -16,9 +18,28 @@ def index(request, script_id, project_name):
         if form.is_valid():
             save_req=form.save(commit=False)
             save_req.save()
-            #form.save()
     else:
         form= ScriptInput(instance=curr_script)
-        #context = {'script_id':script_id, 'script_code':script.code, 'script_name':script.name }
     context={'form':form,'script_id':script_id,'project_name':project_name}
     return render(request, 'script/index.html', context)
+
+def run_script(request, script_id, project_name):
+    script_home='/home/ec2-user/script_repo/'
+    test_output='/home/ec2-user/local_data/'
+    try:
+        curr_script=Script.objects.get(pk=script_id)
+    except:
+        return HttpResponse('Script not found')
+    if request.method=='POST':
+        curr_code=curr_script.code
+        script_name=curr_script.name
+        local_script=script_home+script_name
+        out_data=test_output+'init_test.txt'
+        with open(local_script, 'w+') as s:
+            s.write(curr_code)
+        result= subprocess.run(['python',local_script], stdout=subprocess.PIPE)
+        with open(out_data, 'wb+') as o:
+            o.write(result.stdout)
+        
+    return HttpResponse(curr_code)
+
